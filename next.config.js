8bf58de641aa6fe2d92c6082b008b6affd37625e
cyjo9603 <cyjo9603@naver.com>
2022-07-12 20:53:37 +0900

    chore: 🎉 프로젝트 기본 설정

---
+++ next.config.js
@@ -0,0 +1,50 @@
+const intercept = require('intercept-stdout');
+
+const rewrites =
+  process.env.NODE_ENV === 'development'
+    ? async () => {
+        return {
+          fallback: [
+            {
+              source: '/api/:path*',
+              destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+            },
+            {
+              source: '/guest/api/:path*',
+              destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/guest/api/:path*`,
+            },
+          ],
+        };
+      }
+    : undefined;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites,
+  reactStrictMode: true,
+  webpack(config) {
+    /**
+     * @note https://stackoverflow.com/questions/55175445/cant-import-svg-into-next-js/70961634#comment125790685_70961634
+     * url-loader가 없는 경우
+     * export { ReactComponent as Icon } from './icon.svg -> 사용불가
+     * export { default as Icon } from './icon.svg -> 사용가능 (이렇게 사용한 경우 스토리북에서 오류)
+     */
+    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack', 'url-loader'] });
+    config.module.rules.push({ test: /\.md$/, use: 'raw-loader' });
+
+    return config;
+  },
+};
+
+// safely ignore recoil stdout warning messages
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return '';
+  }
+  return text;
+}
+
+// Intercept in dev and prod
+intercept(interceptStdout);
+
+module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <div>hello beerair</div>;
+};
+
+export default Home;
