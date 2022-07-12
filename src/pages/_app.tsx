1b3773f5d07e888fe36634344dd90d26267b06dd
cyjo9603 <cyjo9603@naver.com>
2022-07-12 22:47:07 +0900

    awesome: 🍻 ^^

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
 import { theme, GlobalStyle } from '@/themes';
+import awesome from '@/utils/awesome';
+
+awesome();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
