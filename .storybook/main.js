67efc5ae3eeccc876b8ead04aa9f77605bfa6866
cyjo9603 <cyjo9603@naver.com>
2022-07-18 21:27:54 +0900

    fix: 🚑 svg webpack5 대응

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  core: {
+    builder: 'webpack5',
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -41,6 +44,12 @@ module.exports = {
       type: 'javascript/auto',
     });
 
+    /**
+     * webpack5 버전 업 이후 svg파일에서 에러가 발생해 아래 블로그 참고
+     * https://minemanemo.tistory.com/161
+     */
+    const fileLoaderRule = config.module.rules.find((rule) => rule?.test?.test?.('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
     config.module.rules.unshift({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
--- next.config.js
+++ next.config.js
@@ -23,13 +23,7 @@ const nextConfig = {
   rewrites,
   reactStrictMode: true,
   webpack(config) {
-    /**
-     * @note https://stackoverflow.com/questions/55175445/cant-import-svg-into-next-js/70961634#comment125790685_70961634
-     * url-loader가 없는 경우
-     * export { ReactComponent as Icon } from './icon.svg -> 사용불가
-     * export { default as Icon } from './icon.svg -> 사용가능 (이렇게 사용한 경우 스토리북에서 오류)
-     */
-    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack', 'url-loader'] });
+    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
     config.module.rules.push({ test: /\.md$/, use: 'raw-loader' });
 
     return config;
