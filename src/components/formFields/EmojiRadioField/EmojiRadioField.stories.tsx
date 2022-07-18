ca16140464187fd3c051759483542e8c6ad7e24e
cyjo9603 <cyjo9603@naver.com>
2022-07-18 21:51:03 +0900

    feat: ✨EmojiRadioField 컴포넌트 이관

---
+++ src/components/formFields/EmojiRadioField/EmojiRadioField.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import EntityForm from '@/components/EntityForm';
+
+import EmojiRadioField from './EmojiRadioField';
+
+export default {
+  title: 'FormFields/EmojiRadioField',
+  component: EmojiRadioField,
+  decorators: [
+    (Story) => (
+      <EntityForm onSubmit={action('onSubmit')}>
+        <div style={{ paddingTop: '80px' }}>
+          <Story />
+        </div>
+      </EntityForm>
+    ),
+  ],
+  argTypes: {
+    className: { control: 'text' },
+  },
+} as ComponentMeta<typeof EmojiRadioField>;
+
+const Template: ComponentStory<typeof EmojiRadioField> = (args) => <EmojiRadioField {...args} />;
+
+export const 이모지_라디오_필드 = Template.bind({});
+이모지_라디오_필드.args = {
+  name: 'emojiRadioField',
+};
---
+++ src/components/formFields/EmojiRadioField/EmojiTransitionViewer.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+import { EmojiValue } from './EmojiRadioGroup';
+
+import Emoji from '@/components/commons/Emoji';
+
+interface EmojiTransitionViewerProps {
+  value: EmojiValue;
+}
+
+const EMOJI_DISPLAY_SIZE = 140;
+
+const FadeInKeyFrames = keyframes`
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const StyledEmojiTransitionViewer = styled(TransitionGroup)`
+  position: relative;
+  width: ${EMOJI_DISPLAY_SIZE}px;
+  height: ${EMOJI_DISPLAY_SIZE}px;
+
+  & .displayed-emoji {
+    position: absolute;
+  }
+
+  & .displayed-emoji-enter {
+    opacity: 0;
+  }
+
+  & .displayed-emoji-enter-active {
+    opacity: 1;
+    transition: opacity 1.2s;
+    animation: ${FadeInKeyFrames} 0.6s forwards;
+  }
+
+  & .displayed-emoji-exit {
+    opacity: 1;
+  }
+
+  & .displayed-emoji-exit-active {
+    transition: opacity 0.4s, transform 0.4s;
+    transform: scale(0);
+    opacity: 0;
+  }
+`;
+
+const EmojiTransitionViewer: React.FC<EmojiTransitionViewerProps> = ({ value }) => {
+  return (
+    <StyledEmojiTransitionViewer>
+      <CSSTransition key={value} timeout={1200} classNames={`displayed-emoji`}>
+        <Emoji feel={value} size={EMOJI_DISPLAY_SIZE} className="displayed-emoji" />
+      </CSSTransition>
+    </StyledEmojiTransitionViewer>
+  );
+};
+
+export default EmojiTransitionViewer;
