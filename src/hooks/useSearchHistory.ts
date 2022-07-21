11c05fc3b20c07ae3eac76ba2eb3b93c364d8d39
Chanyeong Cho <cyjo9603@naver.com>
2022-07-21 10:02:29 +0900

    feat: ✨SearchItem 컴포넌트 이관 (#73)
    * feat: ✨persist recoil effect 이관
    
    * feat: ✨useSearchHistory custom hooks 이관
    
    * feat: ✨RecoilRoot 적용
    
    * feat: ✨SearchItem 컴포넌트 이관

---
+++ src/hooks/useSearchHistory.ts
@@ -0,0 +1,45 @@
+import { useCallback } from 'react';
+import { useRecoilState } from 'recoil';
+import { uniq } from 'lodash';
+
+import { $searchHistories } from '@/recoil/atoms';
+import { config } from '@/constants/config';
+
+const useSearchHistory = () => {
+  const [searchHistories, setSearchHistories] = useRecoilState($searchHistories);
+
+  const addSearchHistory = useCallback(
+    (searchText: string) => {
+      if (!searchText?.trim()) {
+        return;
+      }
+
+      let addedSearchHistories = uniq([searchText, ...searchHistories]);
+
+      if (addedSearchHistories.length > config.history.maxLength) {
+        addedSearchHistories = addedSearchHistories.slice(0, config.history.maxLength);
+      }
+
+      setSearchHistories(addedSearchHistories);
+    },
+    [searchHistories, setSearchHistories],
+  );
+
+  const removeSearchHistory = useCallback(
+    (text: string) => {
+      const removeIndex = searchHistories.findIndex((history) => history === text);
+      if (removeIndex === -1) {
+        return;
+      }
+
+      const removedSearchHistories = [...searchHistories];
+      removedSearchHistories.splice(removeIndex, 1);
+      setSearchHistories(removedSearchHistories);
+    },
+    [searchHistories, setSearchHistories],
+  );
+
+  return { searchHistories, addSearchHistory, removeSearchHistory };
+};
+
+export default useSearchHistory;
