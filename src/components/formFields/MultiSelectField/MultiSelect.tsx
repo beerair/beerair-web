62942fdb9604b568839ae531e866a84d0a7a2595
Chanyeong Cho <cyjo9603@naver.com>
2022-07-31 21:14:47 +0900

    feat: ✨ MultiSelectField 컴포넌트 이관 (#115)
    * feat: ✨ ColorTextBox 컴포넌트 이관
    
    * feat: ✨ MultiSelectField 컴포넌트 이관
    
    * refactor: ♻️ hide scrollbar styles 적용

---
+++ src/components/formFields/MultiSelectField/MultiSelect.tsx
@@ -0,0 +1,128 @@
+import React, { useCallback } from 'react';
+import styled from '@emotion/styled';
+import { isNil } from 'lodash';
+
+import ColorTextBox from '@/components/commons/ColorTextBox';
+import { hideScrollbar } from '@/styles/common';
+
+export interface SelectOption<T = any> {
+  value: T;
+  label: string;
+  key: React.Key;
+  disabled?: boolean;
+}
+
+type ClickEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
+
+type ClickEventHandler<T> = (value: T[] | undefined, event: ClickEvent) => void;
+
+interface MultiSelectProps<T = any> {
+  options: SelectOption<T>[];
+  value: T[];
+  maxLength?: number;
+  height?: string;
+  disabled?: boolean;
+  onChange?: ClickEventHandler<T>;
+  onBlur?: () => void;
+}
+
+interface StyledMultiSelectProps {
+  boxHeight?: string;
+}
+
+const MultiSelect: React.FC<MultiSelectProps> = <T extends any>({
+  options,
+  value,
+  maxLength,
+  height,
+  disabled,
+  onChange,
+  onBlur,
+}: MultiSelectProps<T>) => {
+  const triggerChange = useCallback(
+    (value: T[] | undefined, e: ClickEvent) => {
+      const _value = value?.length !== 0 ? value : undefined;
+
+      if (!disabled) {
+        onChange?.(_value, e);
+        onBlur?.();
+      }
+
+      return _value;
+    },
+    [disabled, onChange, onBlur],
+  );
+
+  const handleClick = useCallback(
+    (clickedValue: T, e: ClickEvent) => {
+      const hasValue = !!value?.includes(clickedValue);
+      const changedValue = [...(value || [])];
+
+      if (hasValue) {
+        const valueIndex = changedValue.findIndex((v) => v === clickedValue);
+        changedValue.splice(valueIndex, 1);
+      } else {
+        if (!isNil(maxLength) && maxLength <= changedValue.length) {
+          return;
+        }
+        changedValue.push(clickedValue);
+      }
+      triggerChange(changedValue, e);
+    },
+    [value, triggerChange, maxLength],
+  );
+
+  return (
+    <StyledMultiSelect boxHeight={height}>
+      <div className="multi-select-list">
+        {options.map((option) => (
+          <ColorTextBox
+            key={option.key}
+            aria-checked={!!value?.includes(option.value)}
+            type={value?.includes(option.value) ? 'primary' : 'default'}
+            size="short"
+            onClick={(e) => handleClick(option.value, e)}
+            className="multi-select-item"
+          >
+            {option.label}
+          </ColorTextBox>
+        ))}
+      </div>
+    </StyledMultiSelect>
+  );
+};
+
+const StyledMultiSelect = styled.div<StyledMultiSelectProps>`
+  position: relative;
+  height: ${({ boxHeight }) => boxHeight || 'fit-content'};
+
+  & > .multi-select-list {
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    overflow-y: scroll;
+
+    & > .multi-select-item {
+      margin-bottom: 16px;
+    }
+
+    ${hideScrollbar}
+  }
+
+  &::after {
+    position: absolute;
+    bottom: 0px;
+    left: 0;
+    height: 20px;
+    width: 100%;
+    content: ' ';
+    background: linear-gradient(
+      180deg,
+      ${({ theme }) => theme.color.whiteOpacity0},
+      ${({ theme }) => theme.color.black100} 100%
+    );
+  }
+`;
+
+export default MultiSelect;
---
+++ src/components/formFields/MultiSelectField/MultiSelectField.tsx
@@ -0,0 +1,51 @@
+import React, { useMemo } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { isNil } from 'lodash';
+
+import MultiSelect, { SelectOption } from './MultiSelect';
+
+interface MultiSelectFieldProps<T = any> {
+  name: string;
+  options: SelectOption<T>[];
+  height?: string;
+  maxLength?: number;
+  required?: boolean;
+}
+
+const MultiSelectField: React.FC<MultiSelectFieldProps> = <T extends any>({
+  name,
+  options,
+  height,
+  maxLength,
+  required = false,
+}: MultiSelectFieldProps<T>) => {
+  const { control } = useFormContext();
+
+  const rules = useMemo(
+    () =>
+      !isNil(maxLength)
+        ? { validate: (value: T[]) => (value?.length || 0) <= maxLength, required }
+        : { required },
+    [maxLength, required],
+  );
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({ field: { value, onChange, onBlur } }) => (
+        <MultiSelect
+          options={options}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          height={height}
+          maxLength={maxLength}
+        />
+      )}
+    />
+  );
+};
+
+export default MultiSelectField;
