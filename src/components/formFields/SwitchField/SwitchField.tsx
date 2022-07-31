6e681c1197c6a26ef8888a1e7cc15a5c64da409c
Chanyeong Cho <cyjo9603@naver.com>
2022-07-31 21:13:40 +0900

    feat: ✨SwitchField 컴포넌트 이관 (#111)
    * feat: ✨Switch 컴포넌트 이관
    
    * feat: ✨SwitchField 컴포넌트 이관

---
+++ src/components/formFields/SwitchField/SwitchField.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import Switch from '@/components/commons/Switch';
+
+interface SelectFieldProps {
+  name: string;
+  className?: string;
+}
+
+const SelectField: React.FC<SelectFieldProps> = ({ name, className }) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { value, onChange } }) => (
+        <Switch onChange={onChange} defaultValue={value} className={className} />
+      )}
+    />
+  );
+};
+
+export default SelectField;
