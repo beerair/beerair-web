c5f35956a341924e4e58678d7843494c5257443b
Chanyeong Cho <cyjo9603@naver.com>
2022-08-03 13:28:02 +0900

    BeerPhotoLabel 컴포넌트 이관 (#119)
    * feat: ✨ sliceAndUpperCase util이관
    
    * test: 🧪storybook dummy 데이터 및 api 연계 전 데이터 표시를 위한 dummy 데이터 추가
    
    * feat: ✨BeerPhotoLabel 컴포넌트 이관
    
    * refactor: :recycle: 사용하지 않는 주석 제거
    
    * refactor: :recycle: styledProps는 styledComponent 바로 상단에 위치

---
+++ src/components/beer/BeerPhotoLabel/BeerPhotoLabel.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { beer } from '@/constants/dummy';
+
+import BeerPhotoLabel from './BeerPhotoLabel';
+
+export default {
+  title: 'Components/beer/BeerPhotoLabel',
+  component: BeerPhotoLabel,
+  argTypes: {
+    beer: { control: 'readonly' },
+    background: { control: 'readonly' },
+  },
+} as ComponentMeta<typeof BeerPhotoLabel>;
+
+const Template: ComponentStory<typeof BeerPhotoLabel> = (args) => <BeerPhotoLabel {...args} />;
+
+export const BasicBeerPhotoLabel = Template.bind({});
+BasicBeerPhotoLabel.args = {
+  beer,
+};
+
+export const BackgroundBeerTicketTitle = Template.bind({});
+BackgroundBeerTicketTitle.args = {
+  beer,
+  background: 'https://beerair-service.s3.ap-northeast-2.amazonaws.com/samples/beer_background.png',
+};
