74e5e4caad2f95f2a6d6a2b7855356b6ad34471e
Ko Seoyoung <laura120555@gmail.com>
2022-08-19 04:25:54 +0900

    feat: ✨ 로그인, 회원가입 페이지 이관  (#133)
    * feat: :lipstick: BeginningLayout wrapper css 수정
    
    * feat: :sparkles: 로그인 페이지 이관
    
    * feat: :sparkles: 회원가입 페이지 이관

---
+++ src/containers/LoginContainer/KakaoLoginButton/KakaoLoginButton.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+const BACKGROUND_COLOR = '#FEE500';
+const TEXT_COLOR = '#191919';
+
+/** @todo api 연동 */
+const KakaoLoginButton = () => {
+  return (
+    <StyledButton>
+      <img src="/images/kakao.png" alt="" />
+      <p>카카오로 시작하기</p>
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.a`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 47px;
+  padding: 0 14px;
+  border-radius: 6px;
+
+  background-color: ${BACKGROUND_COLOR};
+  color: ${TEXT_COLOR};
+
+  > img {
+    width: 18px;
+    height: 18px;
+  }
+
+  > p {
+    flex: 1;
+    font-size: 15px;
+    text-align: center;
+  }
+`;
+
+export default KakaoLoginButton;
---
+++ src/containers/LoginContainer/LoginContainer.stories.tsx
@@ -0,0 +1,10 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+import LoginContainer from './LoginContainer';
+
+export default {
+  title: 'Pages/로그인',
+  component: LoginContainer,
+} as ComponentMeta<typeof LoginContainer>;
+
+export const 로그인: ComponentStoryObj<typeof LoginContainer> = {};
