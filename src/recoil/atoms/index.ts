9a78a88b44e62184c6b869542af9b24f342795ac
syoung125 <laura120555@gmail.com>
2022-08-25 11:36:37 +0900

    feat: :sparkles: beerListViewType recoil atom 추가

--- src/recoil/atoms/index.ts
+++ src/recoil/atoms/index.ts
@@ -1,1 +1,3 @@
+export { default as $beerListViewType, BEER_LIST_VIEW_ATOM_KEY } from './beerListViewType';
+export type { BeerListViewType } from './beerListViewType';
 export { default as $userSession } from './userSession';
