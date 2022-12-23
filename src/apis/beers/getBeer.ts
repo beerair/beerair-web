dbc557d7d2815a617d791ff81c7cedd3e2654be2
Ko Seoyoung <laura120555@gmail.com>
2022-12-24 02:15:36 +0900

    refactor: :recycle: request 함수에 baseURL 설정, useQuery enabled 옵션 설정 (#148)

--- src/apis/beers/getBeer.ts
+++ src/apis/beers/getBeer.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 import { IBaseResponse, IBeer } from '@/types';
+import request from '@/commons/axios';
 
 interface IGetBeerResponseData extends IBaseResponse<IBeer> {}
 
@@ -9,16 +10,19 @@ interface IGetBeerResponseData extends IBaseResponse<IBeer> {}
  * 맥주 상세정보 조회
  */
 export const getBeer = async (beerId: number) => {
-  const res = await axios.get<IGetBeerResponseData>(
-    `https://api.beerair.kr/api/v1/beers/${beerId}`,
-  );
-  return res.data.data;
+  const res = await request<IGetBeerResponseData>({
+    method: 'get',
+    url: `/api/v1/beers/${beerId}`,
+  });
+
+  return res.data;
 };
 
 export const useGetBeer = (beerId: IBeer['id'], initialData?: IBeer) => {
   const result = useQuery(['beer', beerId], () => getBeer(beerId), {
     cacheTime: Infinity,
     initialData,
+    enabled: !!beerId,
   });
 
   return result;
--- src/apis/flavors/getFlavors.ts
+++ src/apis/flavors/getFlavors.ts
@@ -2,20 +2,28 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 import { IBaseResponse, IBeer, IFlavor } from '@/types';
+import request from '@/commons/axios';
 
 export type IGetFlavorsResponseData = IBaseResponse<IFlavor[]>;
 
 export const getFlavors = async (beerId: number, limit: number) => {
-  const res = await axios.get<IGetFlavorsResponseData>(
-    `https://api.beerair.kr/api/v1/flavors/rank?beerId=${beerId}&limit=${limit}`,
-  );
-  return res.data.data;
+  const res = await request<IGetFlavorsResponseData>({
+    method: 'get',
+    url: '/api/v1/flavors/rank',
+    params: {
+      beerId,
+      limit,
+    },
+  });
+
+  return res.data;
 };
 
 export const useGetFlavors = (beerId: IBeer['id'], limit: number, initialData?: IFlavor[]) => {
   const result = useQuery(['flavors', beerId], () => getFlavors(beerId, limit), {
     cacheTime: Infinity,
     initialData,
+    enabled: !!beerId,
   });
 
   return result;
