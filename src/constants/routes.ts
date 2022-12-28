9310bb27713549dcaa71fa4adeb2e9101b6f007b
syoung125 <laura120555@gmail.com>
2022-12-28 17:14:51 +0900

    feat: :sparkles: ROUTE_PATH 상수 추가

---
+++ src/constants/routes.ts
@@ -0,0 +1,29 @@
+export const ROUTE_PATH = {
+  ROOT: '/',
+  HOME: '/home',
+  ONBOARDING: '/onboarding',
+  AUTH: {
+    LOGIN: '/auth/login',
+    SIGNUP: '/auth/signup',
+  },
+  BEERS: {
+    HOME: '/beers',
+    INFO: '/beers/[id]',
+    REQUESTS: {
+      MAIN: '/beers/requests',
+      COMPLETE: '/beers/complete',
+    },
+  },
+  MY: {
+    HOME: '/my',
+    ETC: '/my/etc',
+    PRIVACY_POLICY: '/my/privacy-policy',
+    TERMS: '/my/terms',
+  },
+  RECORDS: {
+    HOME: '/records',
+  },
+  SEARCH: {
+    HOME: '/search',
+  },
+};
