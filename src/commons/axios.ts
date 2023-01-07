0079a3df9e58785d71e60ba8b5f76ed58ff9639b
HyojinKim <gywls00100@gmail.com>
2023-01-07 14:37:53 +0900

    chore: accessToken 업데이트 및 BASE_URL 환경변수 분리 (#154)
    Co-authored-by: cindy <cindy@bemyfriends.com>

--- src/commons/axios.ts
+++ src/commons/axios.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
+const isServer = typeof window === 'undefined';
+
+export const BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
+
 const request = <T>(param: AxiosRequestConfig) => {
   return axios({
-    // TODO: 환경변수로 관리
-    baseURL: 'https://api.beerair.kr',
+    baseURL: BASE_URL,
     ...param,
     headers: {
       ...param.headers,
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAIWNzQrCMBAG32XPRZo0bUlPIvRmEXo1Ipt0xaD9IWlEKX13Q8Gzxx2-mV3ABw0V3KTWTJqUi4yJnDSWrDOcENNOGGQZJDAFN42e4hiNIe8jCp5cvBcFtlNQqb8ZBYkC6tE-t3mH7oHDLOV-wBe5nRl7BWsMY5jvo7OzJR_75_jgRz6b2J6O9bWpm0PdRuESDYszVKwoRJ7nPCsSoPe0gaJMecnT9QtBwel06gAAAA.4RtOtRtTj01Rd4forISWK0lstmB9UzN-qw2UsPNNHIE`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAALWTyW7bMBCG34Vnx6IoWdupaOtLGyCb3VUFQUkji7VICVwCN4bfvTRjN6m7pIf0QEAc_fMNZ9sibStUoGaGSY2jMMVVFad5nmVplbVJStI8ZHVE0ASNVo2DBidmdQ1aO5PVoNx9WyLelKgon8SUaFIiEIz3Xr7uRByR7IVkt6Cm9SD8f2aM4pU1oJ3oyCZRHofRLMOJU9SDlFAbaCgzHkQwIWc4OyN4gfMiTos4-eRZoxpGUIYfWe7e8h4oF2wF3rUzZiyCYD1dszUb6kZOJZigkcGF-PCeBJVRS_Hq42V3VwVXV4v5LF-Ob250lL97fX65eJku3wZcrGgS440706_jysc1nRWVdHk-e6QwxBt37iPtXChPo64ng5Xm5ywlr9eSCfcB0GjKVgpAgFcZZWFyUo_fyFrW60e6e_xJctSq_n8l-MsTnzfWadu4pg20zPbmR998BfZ17pimx9E9VM9f_1I1fvCgt6z3U3zwe7CD4i2H5gT5p-XY7d_BrOkGxQ8z_Xn7YPnm_a4vzud0eTO_dvovTr8fgH9czp3b6Ud4t9pP4J2eM4OK0FGSNMZ5MkGwGZ0hxTEJsTPsvgPaska9YwQAAA.o0mkxuK92JHsMQ36eGbbsFtAJ3Yo4YJooeUer5nUOSc`,
     },
   }).then((response: AxiosResponse<T>) => response.data);
 };
--- src/pages/auth/components/KakaoLoginButton/KakaoLoginButton.tsx
+++ src/pages/auth/components/KakaoLoginButton/KakaoLoginButton.tsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 
+import { BASE_URL } from '@/commons/axios';
+
 const BACKGROUND_COLOR = '#FEE500';
 const TEXT_COLOR = '#191919';
 
 /** @todo api 연동 */
 const KakaoLoginButton = () => {
   return (
-    <StyledButton>
+    <StyledButton href={`${BASE_URL}/oauth2/authorization/kakao`}>
       <img src="/images/kakao.png" alt="" />
       <p>카카오로 시작하기</p>
     </StyledButton>
