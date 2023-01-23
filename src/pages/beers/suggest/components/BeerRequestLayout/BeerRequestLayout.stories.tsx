c7b8b9d6b1bf869ca01ec49d5fdcb735ce1e58d7
syoung125 <laura120555@gmail.com>
2023-01-23 14:48:40 +0900

    refactor: :truck: BeerRequestLayout 이동 (beers/requests -> beers/suggest)

---
+++ src/pages/beers/suggest/components/BeerRequestLayout/BeerRequestLayout.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import BeerRequestLayout from './BeerRequestLayout';
+
+export default {
+  component: BeerRequestLayout,
+  args: {
+    title: '등록할 맥주의 정보를\n입력해 주세요',
+  },
+} as ComponentMeta<typeof BeerRequestLayout>;
+
+const Template: ComponentStory<typeof BeerRequestLayout> = (args) => (
+  <BeerRequestLayout {...args} />
+);
+
+export const 맥주요청_레이아웃 = Template.bind({});
