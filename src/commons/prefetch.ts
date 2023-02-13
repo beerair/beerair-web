e1e4f86e4244157483d5c6640430d930bba9e406
Jihun Oh <hihiwwg@naver.com>
2023-02-13 20:16:35 +0900

    refactor: prefetchWithSSR 함수 수정 (#161)
    * refactor:  prefetchWithSSR function
    
    * fix: next.config.js warning
    
    * fix: type guard
    
    ---------
    
    Co-authored-by: oh.jihoon <oh.jihoon@linecorp.com>

--- src/commons/prefetch.ts
+++ src/commons/prefetch.ts
@@ -1,16 +1,45 @@
-import { dehydrate, QueryClient } from 'react-query';
+import {
+  dehydrate,
+  FetchInfiniteQueryOptions,
+  FetchQueryOptions,
+  QueryClient,
+  QueryFunction,
+  QueryKey,
+  DehydratedState,
+} from 'react-query';
 
-export interface ResourcesType {
-  key: Array<any>;
-  fetcher: (...args: any) => void;
-}
+type ResourcesType<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+> = {
+  key: TQueryKey;
+  fetcher: QueryFunction<TQueryFnData, TQueryKey>;
+} & (
+  | {
+      options?: FetchQueryOptions<TQueryFnData, TError, TData, TQueryKey>;
+      useInfiniteQuery?: false;
+    }
+  | {
+      options?: FetchInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryKey>;
+      useInfiniteQuery: true;
+    }
+);
 
 export async function prefetchWithSSR(resources: ResourcesType[]) {
   const queryClient = new QueryClient();
 
-  await Promise.all(resources.map(({ key, fetcher }) => queryClient.prefetchQuery(key, fetcher)));
+  await Promise.all(
+    resources.map(({ key, fetcher, options, useInfiniteQuery }) => {
+      return useInfiniteQuery
+        ? queryClient.prefetchInfiniteQuery(key, fetcher, options)
+        : queryClient.prefetchQuery(key, fetcher, options);
+    }),
+  );
 
   return {
-    dehydratedState: dehydrate(queryClient),
+    // issue reference : https://github.com/TanStack/query/issues/1458
+    dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))) as DehydratedState,
   };
 }
